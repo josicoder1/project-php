8e31fe71b6cb957f877d32f37065d1ef3080b8cc
sine2112 <sinishawbezabih@gmail.com>
2025-02-02 16:30:48 +0300

    initial commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+document.getElementById("order-city").addEventListener("click", function() {
+    alert("Order Now for City Transport clicked!");
+});
+
+document.getElementById("more-business").addEventListener("click", function() {
+    alert("More for Business Transport clicked!");
+});
+
+document.getElementById("more-corporate").addEventListener("click", function() {
+    alert("More for Corporate Transport clicked!");
+});
+
+document.getElementById("more-tour").addEventListener("click", function() {
+    alert("More for Tour Transport clicked!");
+});
+
+document.getElementById("more-airport").addEventListener("click", function() {
+    alert("More for Airport Transport clicked!");
+});
+
+
+
